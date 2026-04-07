7daf80cc752076883bcfff7478321393dfa972da
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2026-04-07 14:11:35 -0700

    completed part 2.1

---
+++ part2/courseInfo/README.md
@@ -0,0 +1,5 @@
+# Exercises 2.1.-2.5
+
+## 2.1: Course information step 6
+
+Basic course information set-up
---
+++ part2/courseInfo/src/components/Note.jsx
@@ -0,0 +1,9 @@
+const Note = ({note}) => {
+  return (
+    <li key={note.id}>
+      {note.content}
+    </li>
+  )
+}
+
+export default Note
