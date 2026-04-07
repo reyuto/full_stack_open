c0cc424f665121ce9e613ab2077cec5a3c30918d
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2026-04-06 17:13:28 -0700

    completed part 1.1

---
+++ part1/courseinfo/README.md
@@ -0,0 +1,5 @@
+# Exercises for Part 1
+
+## 1.1: Course Information, step 1
+Break the App.jsx component into 3 smaller components **Header**, **Content** and **Total**.
+
---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,61 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const content = props.content;
+
+  return (
+    <div>
+      <p>
+        {content[0].part} {content[0].exercises}
+      </p>
+      <p>
+        {content[1].part} {content[1].exercises}
+      </p>
+      <p>
+        {content[2].part} {content[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const content = [
+    {part: part1, exercises: exercises1},
+    {part: part2, exercises: exercises2},
+    {part: part3, exercises: exercises3},
+  ]
+  
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={content} />    
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
